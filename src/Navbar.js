caa6e7bec1822520ba5e4f5809c89e9626ddef6a
clarkjoey <joeclark3516@gmail.com>
2023-10-07 13:55:58 -0400

    logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,9 +4,6 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <a className="navbar-brand" href="/">
-            <img className="nav-logo" src="/jcdev-logo.jpeg" width="30" height="30" alt=""></img>
-        </a>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
