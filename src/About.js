82eeca9c1cca68e603a032be914288d1fd889289
clarkjoey <joeclark3516@gmail.com>
2023-10-14 20:49:02 -0400

    adjust about section

--- src/About.js
+++ src/About.js
@@ -13,21 +13,21 @@ function About() {
         virtual coding realm. Let me give you a glimpse into my world of
         programming and technology.
       </p>
-      <h4>Who Am I?</h4>
+      <h3>Who Am I?</h3>
       <p>
         I'm a passionate coder hailing from New York, deeply immersed in the
         world of software development. My journey is a symphony of code,
         creativity, and constant learning. It's not as daunting of a journey
         when it's something you genuinely love doing - which I do!
       </p>
-      <h4>My Journey</h4>
+      <h3>My Journey</h3>
       <p>
         In my coding journey I've learned a lot. From getting my Full Stack
         Masters certificate to my hands-on experience working as a Solutions
         Engineer. Every bug squashed, every line of code written has been a
         stepping stone toward mastery.
       </p>
-      <h4>Why This Website?</h4>
+      <h3>Why This Website?</h3>
       <p>
         Welcome to my digital coding playground! This website serves as a
         showcase of my coding portfolio, a place where I can display my
@@ -35,14 +35,14 @@ function About() {
         to the world of programming.
       </p>
 
-      <h4>What to Expect</h4>
+      <h3>What to Expect</h3>
       <p>
         Here, you'll find a plethera of JavaScript projects that I've put a lot
         of time into. From a kanban board to keep me organized at work, to a 3-tier web app that showcases my full stack knowledge, I invite you to explore my
         work and get inspired.
       </p>
 
-      <h4>Let's Connect</h4>
+      <h3>Let's Connect</h3>
       <p>
         I'm always eager to connect with fellow code enthusiasts and
         collaborators. If you have questions, ideas, or just want to chat about
