16af8c65677a6c0136cc5dd1dd7d5fe8e46bbe08
clarkjoey <joeclark3516@gmail.com>
2023-10-16 19:27:02 -0400

    nav mobile optimized

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,8 +5,19 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+        <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
+          <ul className="navbar-nav ml-auto custom-dropdown-menu">
             <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
             </li>
@@ -17,15 +28,22 @@ function Navbar() {
               <Link className="nav-link" to="/contact">Contact</Link>
             </li>
             <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    More
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a className="dropdown-item" href="/">Work</a></li>
-                    <li><a className="dropdown-item" href="/">Education</a></li>
-                    <li><hr className="dropdown-divider" /></li>
-                    <li><a className="dropdown-item" href="/">Donation Links</a></li>
-                </ul>
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                More
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a className="dropdown-item" href="/">Work</a></li>
+                <li><a className="dropdown-item" href="/">Education</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="/">Donation Links</a></li>
+              </ul>
             </li>
           </ul>
         </div>
