3661e9b66c1102f487b6b386754940eb676d22ae
clarkjoey <joeclark3516@gmail.com>
2023-11-19 23:38:43 -0500

    clean up

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <button
           className="navbar-toggler"
